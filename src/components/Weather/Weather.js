162dfa22596d03a52832f301844fbd3e90f9d6cd
rkfr <e.xbyvnqn@gmail.com>
2020-02-20 15:52:20 +0200

    a preferences window and a search field were added to the markup

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import './Weather.scss';
 
 import DropdownWindow from '../DropdownWindow';
+import SettingsDropdown from '../SettingsDropdown';
 import Forecast from '../Forecast';
 
 const Weather = () => {
@@ -27,12 +28,44 @@ const Weather = () => {
       <DropdownWindow
         arrow="top-right"
         isActive={weatherVisibility}
-        className="weather__dropdown"
+        className="weather-dropdown"
       >
         <div className="weather__overview">
-          <h3 className="weather__location-name">
-            Pushcha-Voditsa
-          </h3>
+          <div className="weather__info">
+            <h3 className="weather__location-name">
+              Pushcha-Voditsa
+            </h3>
+            <h4 className="weather__currant-day">
+              Friday
+            </h4>
+            <SettingsDropdown
+              containerClassName="weather__settings"
+              dropdownClassName="weather__settings-window"
+            >
+              <button
+                type="button"
+                className="weather__edit-location-btn"
+              >
+                Edit location
+              </button>
+            </SettingsDropdown>
+            {/* <div className="change-location">
+              <input
+                value="Pushcha-Voditsa"
+                placeholder="Location"
+                type="text"
+                className="change-location__input"
+                onChange={() => {}}
+              />
+              <input
+                type="image"
+                src="./images/close.svg"
+                alt="close button"
+                className="change-location__cancel-btn"
+              />
+            </div> */}
+          </div>
+
           <p className="weather__weather-description">
             Clear sky
           </p>
