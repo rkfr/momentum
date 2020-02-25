28350575bde50345833caa6a180da10d941bb490
rkfr <e.xbyvnqn@gmail.com>
2020-02-25 18:21:14 +0200

    redux was added back

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux';
-import store from './store';
-
 import App from './App';
 import './styles/index.scss';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
