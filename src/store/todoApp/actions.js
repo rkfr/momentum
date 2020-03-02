d63c168bc5a9c07774e4813ce3886d8616c9e5db
rkfr <e.xbyvnqn@gmail.com>
2020-03-02 14:57:14 +0200

    actions and cases were added to edit the item

--- src/store/todoApp/actions.js
+++ src/store/todoApp/actions.js
@@ -3,6 +3,8 @@ export const ACTION_TYPES = {
   ADD_TODO: 'ADD_TODO',
   DELETE_ITEM: 'DELETE_ITEM',
   TOGGLE_COMPLETED: 'TOGGLE_COMPLETED',
+  START_EDITING: 'START_EDITING',
+  SAVE_EDITED: 'SAVE_EDITED',
 };
 
 export const clearList = () => ({
@@ -22,4 +24,14 @@ export const deleteItem = (id) => ({
 export const toggleCompleted = (id) => ({
   type: ACTION_TYPES.TOGGLE_COMPLETED,
   payload: id,
+});
+
+export const startEditing = (id) => ({
+  type: ACTION_TYPES.START_EDITING,
+  payload: id,
+});
+
+export const saveEdited = (itemData) => ({
+  type: ACTION_TYPES.SAVE_EDITED,
+  payload: itemData,
 });
--- src/store/todoApp/reducers.js
+++ src/store/todoApp/reducers.js
@@ -46,6 +46,33 @@ export const todoApp = (state = initialState, action) => {
       };
     }
 
+    case ACTION_TYPES.START_EDITING: {
+      const { todos } = state;
+      const { payload: itemId } = action;
+
+      return {
+        ...state,
+        todos: todos.map((item) => (item.id !== itemId ? item : {
+          ...item,
+          editing: true,
+        })),
+      };
+    }
+
+    case ACTION_TYPES.SAVE_EDITED: {
+      const { todos } = state;
+      const { payload: { id: itemId, text } } = action;
+
+      return {
+        ...state,
+        todos: todos.map((item) => (item.id !== itemId ? item : {
+          ...item,
+          text,
+          editing: false,
+        })),
+      };
+    }
+
     default: {
       return state;
     }
